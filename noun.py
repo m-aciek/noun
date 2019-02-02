16e10bec534a9bebd39046849a1456cad7852fc1
Maciej Olko <maciej.olko@gmail.com>
2019-02-02 03:31:17 +0100

    Remove ALLOWED_CASES, permissive style. Add gitignore.

--- noun.py
+++ noun.py
@@ -1,26 +1,21 @@
 class Noun:
-    ALLOWED_CASES = ('accusative', 'dative', 'ablative', 'genitive', 'vocative', 'locative', 'instrumental')
 
     def __init__(self, nominative, **cases):
         self._dict = {'nominative': nominative}
         for case, value in cases.items():
-            if case in self.ALLOWED_CASES:
-                self._dict[case] = value
-            else:
-                raise TypeError(f"__init__() got an unexpected keyword argument '{case}'")
+            self._dict[case] = value
 
     def __getattr__(self, item):
         try:
+            # TODO retrieve gettext translation
             return self._dict[item]
         except KeyError:
             # TODO invoke context gettext with fallback to nominative translation
-            if item in self.ALLOWED_CASES:
-                return self.nominative
-            raise AttributeError(f'{item} is not allowed noun grammatical case.')
+            return self.nominative
 
 
 if __name__ == '__main__':
-    print(Noun('kaczka', accusative='kaczkę').accusative)
-    print(Noun('pies').accusative)
-    # print(Noun('ble').fakecase)  # raises AttributeError
-    # print(Noun('ble', vocative='bum'))  # raises TypeError
+    print(Noun('kaczka', accusative='kaczkę').accusative)  # kaczkę
+    print(Noun('pies').accusative)  # pies (fallback)
+    print(Noun('ble').fakecase)  # ble (fallback)
+    print(Noun('ble', yetanotherfakecase='bum').yetanotherfakecase)  # bum
